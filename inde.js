f68306eaa043ed268b8bdcc12b57c3c3510b6408
Glenben Ngugi <ngugiglen19@gmail.com>
2024-03-16 12:31:10 +0300

    second commit

---
+++ inde.js
@@ -0,0 +1,20 @@
+document.getElementById('cv').onclick = function() {
+    window.open('asset/CV.pdf', '_blank');
+  };
+//   the'_blank'ensures that the pdf opens in a new page
+document.getElementById('Contacts').onclick = function() {
+    location.href = '/#Contact';//this changes the location of the webpage to the contacts section
+
+};
+document.getElementById('icon1').onclick = function() {
+    location.href = 'https://instagram.com';
+    
+};
+document.getElementById('icon2').onclick = function() {
+    location.href = 'https://twitter.com';
+    
+};
+document.getElementById('icon3').onclick = function() {
+    location.href = 'https://github.com';
+    
+};
